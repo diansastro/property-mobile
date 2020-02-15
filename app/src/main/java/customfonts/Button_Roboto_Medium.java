f4b8d74f201959fe5abffb26be40efe9581e3738
widi <widiyantoko@ymail.com>
2020-02-15 16:32:17 +0800

    add custom fonts

---
+++ app/src/main/java/customfonts/Button_Roboto_Medium.java
@@ -0,0 +1,37 @@
+package customfonts;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+/**
+ * Created by widi on 14/02/20.
+ */
+
+@SuppressLint("AppCompatCustomView")
+public class Button_Roboto_Medium extends Button {
+
+    public Button_Roboto_Medium(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    public Button_Roboto_Medium(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public Button_Roboto_Medium(Context context) {
+        super(context);
+        init();
+    }
+
+    private void init() {
+        if (!isInEditMode()) {
+            Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "fonts/Roboto-Regular.ttf");
+            setTypeface(tf);
+        }
+    }
+}
